e2d481dbcf77153495a5061490065524f7be9ef6
Jay Duong <jayvin.duong@gmail.com>
2024-08-06 21:28:45 -0400

    Linting user defined footnotes

--- tests/linting/token/footnote.test.js
+++ tests/linting/token/footnote.test.js
@@ -14,3 +14,21 @@ test('Standard footnote tokens syntax', () => {
   expect(lint('{{FootnoteEnd}}').getGrade()).toBe(GRADE.PASS)
   expect(lint('{{InsertFootnotes}}').getGrade()).toBe(GRADE.PASS)
 })
+
+test('Invalid syntax for defining footnotes ', () => {
+  expect(lint('{{FootnoteSymbol}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol]}}').getGrade()).toBe(GRADE.ERROR)
+})
+
+test('Defining footnotes symbols', () => {
+  expect(lint('{{FootnoteSymbol[*]}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[†]}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[&dagger;]}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[[*]]}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[[*][†]]}}').getGrade()).toBe(GRADE.ERROR)
+  expect(lint('{{FootnoteSymbol[a]}}').getGrade()).toBe(GRADE.ERROR)
+
+  expect(lint('{{FootnoteSymbol[1]}}').getGrade()).toBe(GRADE.PASS)
+  expect(lint('{{FootnoteSymbol[122]}}').getGrade()).toBe(GRADE.PASS)
+})
